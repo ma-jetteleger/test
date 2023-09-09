0cb7a4f6f02d94a32f0c9d25c22060912dbd627c
Marc-Antoine <ma.jetteleger@gmail.com>
2023-09-08 20:47:33 -0400

    Test commit

--- test/Assets/Branch1/Script1.cs
+++ test/Assets/Branch1/Script1.cs
@@ -7,7 +7,7 @@ public class Script1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int a = 3;
     }
 
     // Update is called once per frame
